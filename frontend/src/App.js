c2991347614cf581373f2d9d4b0b0e831a194996
Tushant <tushantmehta@yahoo.com>
2023-04-13 00:05:27 -0700

    React_app_created

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div >
+      <header >
+        <a href="/">ShopEasy</a>
+        
+      </header>
+      <main>List products</main>
+    </div>
+  );
+}
+
+export default App;

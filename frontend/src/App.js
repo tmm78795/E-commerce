d79fe9e510072026068bab172dc946599c337336
Tushant <tushantmehta@yahoo.com>
2023-04-13 22:55:03 -0700

    Product added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,35 @@
-import logo from './logo.svg';
+import data from './data';
+
 import './App.css';
 
 function App() {
   return (
-    <div >
-      <header >
+    <div>
+      <header>
         <a href="/">ShopEasy</a>
-        
       </header>
-      <main>List products</main>
+      <h1>Featured Products</h1>
+      <div className="products">
+        {data.products.map((product) => (
+          <div className="product" key={product.slug}>
+            <a href={`/product/${product.slug}`}>
+              <img src={product.image} alt={product.name} />
+            </a>
+
+            <div className="product-info">
+              <a href={`/product/${product.slug}`}>
+                <p>{product.name}</p>
+              </a>
+
+              <p>
+                <strong>${product.price}</strong>
+              </p>
+
+              <button>Add to Cart</button>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }

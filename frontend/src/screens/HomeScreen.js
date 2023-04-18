91f90be3d0211bd34d3ffab3de5bd9524bb6f76f
Tushant <tushantmehta@yahoo.com>
2023-04-18 00:23:38 -0700

    Added Reducer to manange states

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,31 +1,58 @@
-import { Link } from "react-router-dom"
+import { Link } from 'react-router-dom';
 
-import { useEffect, useState } from "react"
+import { useEffect, useReducer } from 'react';
+
+import logger from 'use-reducer-logger';
+
+const reducer = (state, action) => {
+  // eslint-disable-next-line default-case
+  switch (action.type) {
+    case 'FETCH_REQUEST':
+      return { ...state, loading: true };
+
+    case 'FETCH_SUCCESS':
+      return { ...state, loading: false, products: action.payload };
+
+    case 'FETCH_FAIL':
+      return { ...state, loading: false, error: action.payload };
+
+    default:
+      return state;
+  }
+};
 
 const HomeScreen = () => {
-
-  const [products, setProducts] = useState([])
+  const [{ loading, error, products }, dispatch] = useReducer(logger(reducer), {
+    loading: true,
+    error: '',
+    products: [],
+  });
   useEffect(() => {
-    
+    dispatch({ type: 'FETCH_REQUEST' });
     const fetchProducts = async () => {
-      const url = "/api/products";
-      const response = await fetch(url)
-      const data = await response.json()
+      try {
+        const url = '/api/products';
+        const response = await fetch(url);
+        const data = await response.json();
 
-      if(response.ok) {
-        setProducts(data)
-       
-      } 
-    }
+        dispatch({ type: 'FETCH_SUCCESS', payload: data });
+      } catch (err) {
+        dispatch({ type: 'FETCH_FAIL', payload: err });
+      }
+    };
 
-    fetchProducts()
-    
-  }, [])
-    return (
-        <div>
-             <h1>Featured Products</h1>
-        <div className="products">
-          {products.map((product) => (
+    fetchProducts();
+  }, []);
+  return (
+    <div>
+      <h1>Featured Products</h1>
+      <div className="products">
+        {loading ? (
+          <div>Loading....</div>
+        ) : error ? (
+          <div>{error}</div>
+        ) : (
+          products.map((product) => (
             <div className="product" key={product.slug}>
               <Link to={`/product/${product.slug}`}>
                 <img src={product.image} alt={product.name} />
@@ -43,10 +70,11 @@ const HomeScreen = () => {
                 <button>Add to Cart</button>
               </div>
             </div>
-          ))}
-        </div>
-        </div>
-    )
-}
+          ))
+        )}
+      </div>
+    </div>
+  );
+};
 
-export default HomeScreen
+export default HomeScreen;

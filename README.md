50da1bc72f2d4d3ddb0f506bbfdb8974ef75503c
Tushant <tushantmehta@yahoo.com>
2023-04-24 23:59:23 -0700

    Added React Bootstrap and Custom Components

--- README.md
+++ README.md
@@ -5,3 +5,5 @@
 1. Created React App
 2. React Route Added
 3. Express added as backend
+4. Added react bootstrap
+5. Added manual components
---
+++ frontend/src/components/Product.js
@@ -0,0 +1,29 @@
+import Card from 'react-bootstrap/Card';
+import { Link } from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
+
+import Rating from "./Rating"
+
+const Product = (props) => {
+  const {product}  = props;
+  return (
+    <Card >
+      <Link to={`/product/${product.slug}`}>
+        <img src={product.image} className="card-img-top" alt={product.name} />
+      </Link>
+      <Card.Body>
+        <Link to={`/product/${product.slug}`}>
+          <Card.Title>{product.name}</Card.Title>
+        </Link>
+        <Rating rating={product.rating} numReviews={product.numReviews}></Rating>
+        <Card.Text>
+          <strong>${product.price}</strong>
+        </Card.Text>
+
+        <Button>Add to Cart</Button>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default Product;
